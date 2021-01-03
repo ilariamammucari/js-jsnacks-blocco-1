501b692e7f136fd4075c81524b75628cb142b158
Ilaria Mammucari <ilariamammucari@hotmail.it>
2021-01-03 15:49:32 +0100

    somma dei numeri inseriti

---
+++ snack-9/js/script.js
@@ -0,0 +1,20 @@
+// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+
+var numeri = [];
+var somma = 0;
+var lettere = ['a','b','c','d','e','f','g','h','i','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','k','j'];
+var semaforo = true;
+
+while ((somma != 50) && semaforo){
+  var utente = parseInt(prompt('Inserisci un numero'));
+  numeri.push(utente);
+  somma += utente;
+
+  if (somma > 50){
+    semaforo = false;
+    console.log('Hai superato il totale di 50, la somma dei tuoi numeri è di:', somma);
+  } else if (somma == 50) {
+    console.log('La somma dei numeri che hai inserito è di 50!');
+  }
+} 
+console.log(numeri);
